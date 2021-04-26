15c7600a22009ea9ac3b5e66491f2098a0f40f15
lenardsilvester <72597718+lenardsilvester@users.noreply.github.com>
2021-04-26 13:37:00 +0200

    made a navbar

--- src/index.php
+++ src/index.php
@@ -1,25 +1,6 @@
-<!DOCTYPE HTML>
-<html lang="nl">
-  <head>
-    <title>plantperfect</title>
+<?php
+require "includes/layouts/header.php";
+?>
+<div class="cards">
 
-    <!-- meta data -->
-    <meta charset="UTF-8">
-    <meta name="author" content="lenardsilvester">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:site_name" content="plantperfect" />
-    <meta property="og:title" content="plantperfect webshop." />
-    <meta property="og:url" content="https://lenardsilvester.nl/" />
-    <meta property="og:description" content="&copy; <?php echo date("Y"); ?> - lenardsilvester." />
-
-    <!-- icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-
-    <!-- css stylesheets -->
-    <link rel="stylesheet" href="css/style.css">
-
-    <!-- font: poppins -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet">
-
-  </head>
+</div>
